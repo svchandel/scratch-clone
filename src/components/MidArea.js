601766e89b93a3ecd49e3fb1d3b75250f41ac53d
svchandel <svchandel@outlook.com>
2021-07-12 14:39:05 +0530

    Updated Instructions

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -3,6 +3,9 @@ import React from "react";
 const MidArea = ({class1,dragstart,dragover,ondrop}) => {
   return (
     <div className="flex-1 h-screen w-screen overflow-auto border-r">
+    {"Directions-"}
+    {"Select any Function from Left and then press the Play button on right above the cat, buttons are draggable"} 
+    
       <div draggable style={{height:"80vh",width:"100%",margin:"50px auto"}} onDragStart={dragstart} onDragOver={dragover} 
       onDrop={ondrop} className={class1} id="droppedarea">
       </div>
